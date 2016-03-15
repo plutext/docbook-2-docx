306382dedb7276358a884dc1d9bc6b58dbcb0969
sali <sali2@loyalty.com>
2016-03-15 08:08:59 -0400

    adding AbstractParaBuilder to handle paragraphs

--- docx-builder/src/main/java/com/alphasystem/docbook/builder/impl/block/ParaBuilder.java
+++ docx-builder/src/main/java/com/alphasystem/docbook/builder/impl/block/ParaBuilder.java
@@ -1,13 +1,18 @@
 package com.alphasystem.docbook.builder.impl.block;
 
 import com.alphasystem.docbook.builder.Builder;
-import com.alphasystem.docbook.builder.impl.BlockBuilder;
 import org.docbook.model.Para;
+import org.docx4j.wml.P;
+
+import java.util.ArrayList;
+import java.util.List;
+
+import static com.alphasystem.util.AppUtil.isInstanceOf;
 
 /**
  * @author sali
  */
-public class ParaBuilder extends BlockBuilder<Para> {
+public class ParaBuilder extends AbstractParaBuilder<Para> {
 
     public ParaBuilder(Builder parent, Para obj) {
         super(parent, obj);
@@ -18,4 +23,16 @@ public class ParaBuilder extends BlockBuilder<Para> {
         content = source.getContent();
     }
 
+    @Override
+    protected List<Object> postProcess(List<Object> processedTitleContent, List<Object> processedChildContent) {
+        List<Object> result = new ArrayList<>();
+        processedChildContent.forEach(o -> {
+            if (isInstanceOf(P.class, o)) {
+                result.add(addParaProperties((P) o));
+            } else {
+                result.add(o);
+            }
+        });
+        return result;
+    }
 }
--- docx-builder/src/main/java/com/alphasystem/docbook/builder/impl/block/SimpleParaBuilder.java
+++ docx-builder/src/main/java/com/alphasystem/docbook/builder/impl/block/SimpleParaBuilder.java
@@ -1,12 +1,8 @@
 package com.alphasystem.docbook.builder.impl.block;
 
 import com.alphasystem.docbook.builder.Builder;
-import com.alphasystem.docbook.builder.impl.BlockBuilder;
-import com.alphasystem.openxml.builder.wml.PPrBuilder;
-import com.alphasystem.openxml.builder.wml.WmlAdapter;
 import org.docbook.model.SimplePara;
 import org.docx4j.wml.P;
-import org.docx4j.wml.PPr;
 
 import java.util.Collections;
 import java.util.List;
@@ -14,7 +10,7 @@ import java.util.List;
 /**
  * @author sali
  */
-public class SimpleParaBuilder extends BlockBuilder<SimplePara> {
+public class SimpleParaBuilder extends AbstractParaBuilder<SimplePara> {
 
     public SimpleParaBuilder(Builder parent, SimplePara simplePara) {
         super(parent, simplePara);
@@ -26,21 +22,8 @@ public class SimpleParaBuilder extends BlockBuilder<SimplePara> {
     }
 
     @Override
-    protected void preProcess() {
-        final PPr ppr = ((BlockBuilder) parent).getParaProperties();
-        if(ppr != null) {
-            paraProperties = new PPrBuilder(ppr, paraProperties).getObject();
-        }
-    }
-
-    @Override
     protected List<Object> postProcess(List<Object> processedTitleContent, List<Object> processedChildContent) {
-        final P p = (P) processedChildContent.get(0);
-        if (paraProperties != null) {
-            p.setPPr(paraProperties);
-        }
-        WmlAdapter.addBookMark(p, source.getId());
-        return Collections.singletonList(p);
+        return Collections.singletonList(addParaProperties((P) processedChildContent.get(0)));
     }
 
 }
