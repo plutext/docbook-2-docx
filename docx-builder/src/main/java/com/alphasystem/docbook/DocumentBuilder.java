8b4d2a692bb3c524932a1d88b77f415d025ead44
sfali23 <f.syed.ali@gmail.com>
2017-02-27 11:47:14 -0500

    fix compilation

--- docx-builder/src/main/java/com/alphasystem/docbook/DocumentBuilder.java
+++ docx-builder/src/main/java/com/alphasystem/docbook/DocumentBuilder.java
@@ -20,7 +20,12 @@ import org.docx4j.openpackaging.parts.WordprocessingML.StyleDefinitionsPart;
 import org.docx4j.wml.Style;
 import org.docx4j.wml.Styles;
 
-import java.io.*;
+import java.io.ByteArrayOutputStream;
+import java.io.File;
+import java.io.IOException;
+import java.io.InputStream;
+import java.io.Reader;
+import java.io.StringWriter;
 import java.nio.file.Files;
 import java.nio.file.Path;
 import java.util.HashMap;
@@ -112,7 +117,7 @@ public class DocumentBuilder {
         ApplicationController.startContext(documentContext);
         WordprocessingMLPackage wordprocessingMLPackage = null;
         try {
-            WmlPackageBuilder wmlPackageBuilder = new WmlPackageBuilder(configurationUtils.getTemplate())
+            WmlPackageBuilder wmlPackageBuilder = WmlPackageBuilder.createPackage(configurationUtils.getTemplate())
                     .styles(configurationUtils.getStyles());
             wordprocessingMLPackage = wmlPackageBuilder.getPackage();
             MainDocumentPart mainDocumentPart = wordprocessingMLPackage.getMainDocumentPart();
